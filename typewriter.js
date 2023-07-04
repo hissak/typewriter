21d84f794129ce03a1a83ceb2eae7a3d015b128b
vagrant <hassan.a.issak@gmail.com>
2023-07-04 19:19:41 +0000

    created typewriter function

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs";
+const typewriter = function (string) {
+  let timer = 0;
+  for (const char of string) {
+    setTimeout(() => {
+      process.stdout.write(`${char}`)
+    }, timer);
+    timer += 50
+  } 
+  setTimeout(() => {
+    console.log("\n")
+  }, 2000)
+}
+
+typewriter(sentence)
+
+
